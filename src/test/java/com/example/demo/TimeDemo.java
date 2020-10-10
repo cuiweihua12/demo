6314c505a3a7f2860aeef6232b6ebbc74b5ad11f
cuiweihua <cuiweihua12@163.com>
2020-10-10 11:30:59 +0800

    add TimeTest

---
+++ src/test/java/com/example/demo/TimeDemo.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author cwh
+ * @date 2020/10/10 9:13
+ */
+public class TimeDemo {
+
+    @Test
+    public void test(){
+        /*System.out.println(System.currentTimeMillis());*/
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(simpleDateFormat.format(new Date()));
+
+
+        /*
+        System.out.println(new Date());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());*/
+    }
+
+}
