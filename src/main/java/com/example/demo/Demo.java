f09523614f96958beb42bf0627674f908a834133
cuiweihua <cuiweihua@sjgtw.com>
2021-02-20 09:34:51 +0800

    新增OpenSSL加签验签

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
 /**
  * @author cwh
  * @date 2020/10/9 17:01
@@ -7,6 +11,13 @@ package com.example.demo;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(0/10);
+        //时间戳
+        Timestamp timestamp = Timestamp.valueOf(LocalDateTime.now());
+        System.out.println(timestamp);
+        System.out.println(timestamp.getTime());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String format = simpleDateFormat.format(timestamp.getTime());
+        System.out.println(format);
     }
 
 
